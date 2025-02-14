c0f8bdbf1a890f333dd46b705d8b5b9c0fad1a09
maraki696 <natanjordan2019@gmail.com>
2025-02-14 08:10:49 -1200

    server.js

--- server.js
+++ server.js
@@ -43,6 +43,15 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 app.use(express.json());
 
+app.get("/check-env", (req, res) => {
+  res.json({
+    DB_HOST: process.env.DB_HOST,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD ? "HIDDEN" : "MISSING",
+    DB_NAME: process.env.DB_NAME,
+    DB_PORT: process.env.DB_PORT,
+  });
+});
 
 
 
