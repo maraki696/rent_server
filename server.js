a1c90944473c8d5535732f3f4f6bd3d91816b37d
unknown <natanjordan2019@gmail.com>
2025-02-14 13:47:08 -1200

    Fix login issue

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
 const path = require("path");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 require("dotenv").config();
 
 
